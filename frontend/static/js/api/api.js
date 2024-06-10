f27a8d48f2ead4b58f8a520b35dc926c353df5bb
Misung Kim <misung178@gmail.com>
2024-06-10 14:33:25 +0900

    Feature: 언어변경 API 적용

--- frontend/static/js/api/api.js
+++ frontend/static/js/api/api.js
@@ -92,3 +92,22 @@ export const patchAvatar = async (imgId) => {
     console.log(error);
   }
 }
+
+export const patchLanguage = async (lang) => {
+  try {
+    const response = await fetch('http://localhost:8000/profile/language/', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ language: lang }),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
