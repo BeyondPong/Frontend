40cae7bce6e1402fbee7acdc8bd9c79366a23c50
Sungho <shson1217@kw.ac.kr>
2024-06-13 17:03:48 +0900

    Feature: 2FA인증 체크 함수

--- frontend/static/js/utility/check2FA.js
+++ frontend/static/js/utility/check2FA.js
@@ -1,9 +1,6 @@
 export const check2FAStatus = () => {
-  const token = localStorage.getItem('token');
   const twoFAStatus = localStorage.getItem('2FA');
-
-  if ((token && twoFAStatus === 'pending') || twoFAStatus === null) {
-    window.location.href = '/2fa';
+  if (twoFAStatus === null) {
     return false;
   }
   return true;
