c315d6732d85512c9cbd78ca6a83ad72efa85ad3
Sungho <shson1217@kw.ac.kr>
2024-06-17 16:48:16 +0900

    Feature: 2FA로그인 api 추가

--- frontend/static/js/api/api.js
+++ frontend/static/js/api/api.js
@@ -1,9 +1,52 @@
+export const postLoginCode2FA = async (code) => {
+  try {
+    const response = await fetch('http://localhost:8000/login/two_fa/verify/', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer' + ' ' + localStorage.getItem('token'),
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ verification_code: code }),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    alert('Failed to get 2FA code: ', error);
+    return null;
+  }
+};
+
+export const postLogin2FA = async (email) => {
+  try {
+    const response = await fetch('http://localhost:8000/login/two_fa/request/', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer' + ' ' + localStorage.getItem('token'),
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: email }),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    alert('Failed to get 2FA code: ', error);
+    window.location.href = '/';
+    console.log('Failed to get 2FA code: ', error);
+  }
+};
+
 export const getRoomName = async (mode) => {
   try {
     const response = await fetch(`http://localhost:8000/play/room/?mode=${mode}`, {
       method: 'GET',
       headers: {
-        Authorization: 'Bearer' + ' ' + localStorage.getItem('token'),
+        Authorization: 'Bearer' + ' ' + localStorage.getItem('2FA'),
       },
     });
     if (!response.ok) {
@@ -21,7 +64,7 @@ export const getProfileData = async () => {
     const response = await fetch('http://localhost:8000/profile/information/', {
       method: 'GET',
       headers: {
-        Authorization: 'Bearer' + ' ' + localStorage.getItem('token'),
+        Authorization: 'Bearer' + ' ' + localStorage.getItem('2FA'),
       },
     });
     if (!response.ok) {
@@ -38,7 +81,7 @@ export const getHistoryData = async () => {
   try {
     const response = await fetch('http://localhost:8000/profile/history/', {
       headers: {
-        Authorization: 'Bearer' + ' ' + localStorage.getItem('token'),
+        Authorization: 'Bearer' + ' ' + localStorage.getItem('2FA'),
       },
     });
     if (!response.ok) {
@@ -55,7 +98,7 @@ export const getFriendsData = async () => {
   try {
     const response = await fetch('http://localhost:8000/profile/friends/', {
       headers: {
-        'Authorization': 'Bearer' + ' ' + localStorage.getItem('token'),
+        Authorization: 'Bearer' + ' ' + localStorage.getItem('2FA'),
       },
     });
     if (!response.ok) {
@@ -66,13 +109,13 @@ export const getFriendsData = async () => {
   } catch (error) {
     console.log('Failed to load history data: ', error);
   }
-}
+};
 
 export const getSearchResultData = async (userId) => {
   try {
     const response = await fetch(`http://localhost:8000/profile/search/?nickname=${userId}`, {
       headers: {
-        Authorization: 'Bearer' + ' ' + localStorage.getItem('token'),
+        Authorization: 'Bearer' + ' ' + localStorage.getItem('2FA'),
       },
     });
     if (!response.ok) {
@@ -90,7 +133,7 @@ export const deleteFriend = async (userId) => {
     const response = await fetch(`http://localhost:8000/profile/friends/${userId}/`, {
       method: 'DELETE',
       headers: {
-        'Authorization': 'Bearer' + ' ' + localStorage.getItem('token'),
+        Authorization: 'Bearer' + ' ' + localStorage.getItem('2FA'),
       },
     });
     if (!response.ok) {
@@ -101,7 +144,7 @@ export const deleteFriend = async (userId) => {
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 export const postAddFriend = async (userId) => {
   try {
@@ -109,7 +152,7 @@ export const postAddFriend = async (userId) => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: 'Bearer' + ' ' + localStorage.getItem('token'),
+        Authorization: 'Bearer' + ' ' + localStorage.getItem('2FA'),
       },
     });
     if (!response.ok) {
@@ -128,7 +171,7 @@ export const postLoginCode = async (code) => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: 'Bearer' + ' ' + localStorage.getItem('token'),
+        Authorization: 'Bearer' + ' ' + localStorage.getItem('2FA'),
       },
       body: JSON.stringify({ code: code }),
     });
@@ -148,7 +191,7 @@ export const patchStatusMessage = async (message) => {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: 'Bearer' + ' ' + localStorage.getItem('token'),
+        Authorization: 'Bearer' + ' ' + localStorage.getItem('2FA'),
       },
       body: JSON.stringify({ status_msg: message }),
     });
@@ -167,7 +210,7 @@ export const patchAvatar = async (imgId) => {
     const response = await fetch('http://localhost:8000/profile/information/photo/', {
       method: 'PATCH',
       headers: {
-        Authorization: 'Bearer' + ' ' + localStorage.getItem('token'),
+        Authorization: 'Bearer' + ' ' + localStorage.getItem('2FA'),
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ profile_img: imgId }),
@@ -187,7 +230,7 @@ export const patchLanguage = async (lang) => {
     const response = await fetch('http://localhost:8000/profile/language/', {
       method: 'PATCH',
       headers: {
-        Authorization: 'Bearer' + ' ' + localStorage.getItem('token'),
+        Authorization: 'Bearer' + ' ' + localStorage.getItem('2FA'),
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ language: lang }),
